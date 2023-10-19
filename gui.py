660650f18718dbec9a78d5c5bf70c630554bb6c7
elias <eliasperras@outlook.de>
2023-10-19 23:26:39 +0200

    .

--- gui.py
+++ gui.py
@@ -16,6 +16,13 @@ VERSION_PATCH = 0
 MAIN_WINDOW_SIZE_X = 1200
 MAIN_WINDOW_SIZE_Y = 800
 
+# Geometry Window
+GEOM_WINDOW_SIZE_X = 1200
+GEOM_WINDOW_SIZE_Y = 800
+
+# Standard Canvas Size
+CANVAS_SIZE_X = 900
+CANVAS_SIZE_Y = 700
 #################################################
 
 class GUI(tk.Tk):
@@ -24,42 +31,53 @@ class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
 
-
         # FONTS
         self.STANDARD_FONT_1_BOLD = tkFont.Font(family="Arial", size=12, weight='bold')
+
+        # Start Main Window
         self.main_window()
 
-
-
+        # main input and output variables
+        self.geometry_input = None
 
     def main_window(self):
         """
 
         :return:
         """
-        self.title('TinyFEM')
+        self.title('TinyFEM - MAIN WINDOW')
         self.geometry(f"{MAIN_WINDOW_SIZE_X}x{MAIN_WINDOW_SIZE_Y}")
 
         button_define_geometry = tk.Button(self, text="DEFINE GEOMETRY", command=self.define_geometry, width=20, height=1)
         button_define_geometry.place(relx=0.025, rely=0.05)
 
+        self.text_label = tk.Label(self, text="Init", font=self.STANDARD_FONT_1_BOLD)
+        self.text_label.place(relx=0.4, rely=0.4)
+
     def define_geometry(self):
-        return_geometry = Geometry()
-        print(return_geometry)
+        return_geometry = Geometry(self.receive_geometry)
+
+    def receive_geometry(self, geometry):
+        self.geometry_input = str(geometry)
+        self.text_label.config(text=self.geometry_input)
 
 
-class Geometry(GUI):
+class Geometry(GUI, tk.Toplevel):
 
-    def __init__(self):
+    def __init__(self, callback_geometry):
         super().__init__()
+
+        # Callback geometry to return geometry values to guimain
+        self.callback_geometry = callback_geometry
+        self.geometry_input = None
 
     def main_window(self):
         """
 
         :return:
         """
-        self.title('Define Geometry')
-        self.geometry(f"{MAIN_WINDOW_SIZE_X}x{MAIN_WINDOW_SIZE_Y}")
+        self.title('TinyFEM - DEFINE GEOMETRY')
+        self.geometry(f"{GEOM_WINDOW_SIZE_X}x{GEOM_WINDOW_SIZE_Y}")
 
         button_clear = tk.Button(self, text="Geometry", command=self.return_geometry,
                                  font=self.STANDARD_FONT_1_BOLD, width=10, height=1)
@@ -68,9 +86,10 @@ class Geometry(GUI):
 
     def return_geometry(self):
         value = 999
-        return value
+        self.callback_geometry(value)
 
 
 if __name__ == '__main__':
-    gui = GUI()
+    #gui = GUI()  # Todo - Develop: For testing main gui
+    gui = Geometry(0)  # Todo - Develop: For testing Geometry gui
     gui.mainloop()
--- test1.py
+++ test1.py
@@ -1,51 +1,65 @@
 import tkinter as tk
-from tkinter import font as tkFont
+import tkinter.font as tkFont
 
-MAIN_WINDOW_SIZE_X = 400
-MAIN_WINDOW_SIZE_Y = 300
+# Main Window
+MAIN_WINDOW_SIZE_X = 1200
+MAIN_WINDOW_SIZE_Y = 800
 
-class GUIMain(tk.Tk):
+class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.x = 100
+
         # FONTS
         self.STANDARD_FONT_1_BOLD = tkFont.Font(family="Arial", size=12, weight='bold')
+
+        # Initialize the instance variable
+        self.text_variable = None
+
+        # Start Main Window
         self.main_window()
 
     def main_window(self):
+        print("first", self.text_variable)
         self.title('TinyFEM')
         self.geometry(f"{MAIN_WINDOW_SIZE_X}x{MAIN_WINDOW_SIZE_Y}")
+
+        # Create a Label to display the instance variable
+        self.text_label = tk.Label(self, text="", font=self.STANDARD_FONT_1_BOLD)
+        self.text_label.place(relx=0.4, rely=0.4)
 
         button_define_geometry = tk.Button(self, text="DEFINE GEOMETRY", command=self.define_geometry, width=20, height=1)
         button_define_geometry.place(relx=0.025, rely=0.05)
 
     def define_geometry(self):
-        return_geometry = Geometry(self.receive_geometry_value)
+        # Create an instance of the Geometry class, passing a callback function
+        return_geometry = Geometry(self.receive_geometry)
 
-    def receive_geometry_value(self, value):
-        print("Received value from Geometry:", value)
+    def receive_geometry(self, geometry):
+        # Update the instance variable with the received value
+        self.text_variable = str(geometry)
+        # Update the Label with the new value
+        self.text_label.config(text=self.text_variable)
+        print("second", self.text_variable)
 
-class Geometry(GUIMain, tk.Toplevel):
-    def __init__(self, callback):
+class Geometry(GUI, tk.Toplevel):
+    def __init__(self, callback_geometry):
         super().__init__()
 
-        # Store the callback function
-        self.callback = callback
-        self.main_window()
+        # Callback geometry to return geometry values to guimain
+        self.callback_geometry = callback_geometry
 
     def main_window(self):
-        print(self.x)
         self.title('Define Geometry')
         self.geometry(f"{MAIN_WINDOW_SIZE_X}x{MAIN_WINDOW_SIZE_Y}")
 
-        button_clear = tk.Button(self, text="Geometry", command=self.return_geometry, width=10, height=1)
+        button_clear = tk.Button(self, text="Geometry", command=self.return_geometry,
+                                 font=self.STANDARD_FONT_1_BOLD, width=10, height=1)
         button_clear.place(relx=0.025, rely=0.05)
 
     def return_geometry(self):
         value = 999
-        # Call the callback function in the GUI class to pass the value
-        self.callback(value)
+        self.callback_geometry(value)
 
-if __name__ == "__main__":
-    app = GUIMain()
-    app.mainloop()
+if __name__ == '__main__':
+    gui = GUI()
+    gui.mainloop()
