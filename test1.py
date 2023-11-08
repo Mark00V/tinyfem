894c071ab4d97757bc730cf1ea5564cc75c74b6b
elias <eliasperras@outlook.de>
2023-11-09 00:43:46 +0100

    Added tracer to boundary type/ Icon as bytestring

--- test1.py
+++ test1.py
@@ -1,4 +1,43 @@
-import numpy as np
+from PIL import Image
+import pytesseract
 
-x = np.zeros((2, 1), dtype=np.single)
-print(x)
+# Open an image using Pillow (PIL)
+image = Image.open("tiny_fem_icon.ico")
+
+# Perform OCR using pytesseract
+text = pytesseract.image_to_string(image)
+
+# Print the extracted text
+print(text)
+
+
+############################
+
+from PIL import Image
+import io
+
+# Assuming 'byte_data' contains your byte sequence
+byte_data = b'\x89PNG\r\n...'
+
+# Create an image from the byte sequence
+img = Image.open(io.BytesIO(byte_data))
+
+# Save the image as a PNG file
+img.save("icon.png", "PNG")
+
+###########################
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("My App")
+
+# Load the image as an icon
+icon_image = tk.PhotoImage(file="icon.png")
+
+# Set the icon for the tkinter window
+root.tk.call('wm', 'iconphoto', root._w, icon_image)
+
+# Rest of your tkinter application code here
+
+root.mainloop()
